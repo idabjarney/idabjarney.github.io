2205e4e0b9ba5ad0752b5c980c5f4694a3c0c1d1
idaism <ida.bjarney@outlook.com>
2023-08-04 17:57:44 +0100

    added JS for opening, closing the dropdown menus

--- src/header.ts
+++ src/header.ts
@@ -4,9 +4,12 @@ const navWrapper: HTMLDivElement | null = document.querySelector('.nav-module-wr
 const nav: HTMLDivElement | null = document.querySelector('.nav-module');
 const workflowBtn: HTMLAnchorElement | null = document.querySelector('.workflow-dropdown-btn');
 const workflowList: HTMLUListElement | null = document.querySelector('.workflow-ul');
+const popups: NodeListOf<HTMLElement> | null = document.querySelectorAll('.popup');
+const currencyBtn: HTMLDivElement | null = document.querySelector('.current-currency-btn');
+const currencyContainer: HTMLDivElement | null = document.querySelector('.currency-picker-container');
 
 menuBtn?.addEventListener('click', function(): void {
-  navWrapper?.classList.add('show');
+  toggleElement(navWrapper)
 })
 
 closeMenuBtn?.addEventListener('click', function(): void {
@@ -20,8 +23,30 @@ navWrapper?.addEventListener('click', function(e): void {
   }
 })
 
-workflowBtn?.addEventListener('click', function(): void {
-  // workflowList?.classList.add('show');
-  console.log('click')
+workflowBtn?.addEventListener('click', function(e): void {
+  e.preventDefault();
+  toggleElement(workflowList);
 })
+
+currencyBtn?.addEventListener('click', function(): void {
+  // if (currencyContainer?.className.includes('show')) 
+  toggleElement(currencyContainer);
+})
+
+
+
+window.addEventListener('click', ({target}) => {
+  const popup = (target as HTMLElement).closest('.popup');
+  const clickedOnClosedPopup = popup && !popup.classList.contains('show');
+  if ((target as HTMLElement).className.includes('workflow-dropdown-btn') || (target as HTMLDivElement).className.includes('current-currency-btn')) return;
+  Array.from(popups).forEach(p => p.classList.remove('show'));
+  if (clickedOnClosedPopup) popup.classList.add('show');
+})
+
+function toggleElement(element: any): void {
+  element.classList.toggle('show');
+}
+
+
+
 
